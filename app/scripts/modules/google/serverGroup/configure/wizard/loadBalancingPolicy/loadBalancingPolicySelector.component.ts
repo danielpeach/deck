d6144c52ec3ec4021bbc546518fc55442cdf4404
Daniel Peach <dpeach@google.com>
2016-11-14 17:51:02 -0500

    (google) include CONNECTION option for ssl load balancers

--- app/scripts/modules/google/serverGroup/configure/wizard/loadBalancingPolicy/loadBalancingPolicySelector.component.ts
+++ app/scripts/modules/google/serverGroup/configure/wizard/loadBalancingPolicy/loadBalancingPolicySelector.component.ts
@@ -1,4 +1,4 @@
-import {set} from 'lodash';
+import {set, get} from 'lodash';
 import {module} from 'angular';
 import './loadBalancingPolicySelector.component.less';
 
@@ -17,19 +17,55 @@ class GceLoadBalancingPolicySelectorController implements ng.IComponentControlle
   };
 
   public onBalancingModeChange (mode: string): void {
-    if (mode === 'RATE') {
-      delete this.command.loadBalancingPolicy.maxUtilization;
-    } else if (mode === 'UTILIZATION') {
-      delete this.command.loadBalancingPolicy.maxRatePerInstance;
+    let keys: string[] = ['maxUtilization', 'maxRatePerInstance', 'maxConnectionsPerInstance'];
+    let toDelete: string[] = [];
+    switch (mode) {
+      case 'RATE':
+        toDelete = _.without(keys, 'maxRatePerInstance');
+        break;
+      case 'UTILIZATION':
+        toDelete = _.without(keys, 'maxUtilization');
+        break;
+      case 'CONNECTION':
+        toDelete = _.without(keys, 'maxConnectionsPerInstance');
+        break;
+      default:
+        break;
     }
+
+    toDelete.forEach((key) => delete this.command.loadBalancingPolicy[key]);
   }
 
-  public $onInit (): void {
-    if (!this.command.loadBalancingPolicy) {
-      this.command.loadBalancingPolicy = {
-        balancingMode: 'UTILIZATION'
-      };
+  public getBalancingModes (): string[] {
+    let balancingModes: string[] = [];
+    /*
+    * Four cases:
+    *   - If we have only HTTP(S) load balancers, our balancing mode can be RATE or UTILIZATION.
+    *   - If we have only SSL load balancers, our balancing mode can be CONNECTION or UTILIZATION.
+    *   - If we have both, only UTILIZATION.
+    *   - If we have only SSL load balancers and an autoscaling policy, only UTILIZATION.
+    * */
+    if (_.has(this, 'command.backingData.filtered.loadBalancerIndex')) {
+      let index = this.command.backingData.filtered.loadBalancerIndex;
+      let selected = this.command.loadBalancers;
+
+      let hasSsl = selected.find((loadBalancer: any) => get(index[loadBalancer], 'loadBalancerType') === 'SSL');
+      let hasHttp = selected.find((loadBalancer: any) => get(index[loadBalancer], 'loadBalancerType') === 'HTTP');
+      let hasAutoscalingPolicy = this.command.autoscalingPolicy;
+
+      if ((hasSsl && hasHttp) || (hasSsl && hasAutoscalingPolicy)) {
+        balancingModes = ['UTILIZATION'];
+      } else if (hasSsl) {
+        balancingModes = ['CONNECTION', 'UTILIZATION'];
+      } else {
+        balancingModes = ['RATE', 'UTILIZATION'];
+      }
     }
+
+    if (!balancingModes.includes(get(this.command, 'loadBalancingPolicy.balancingMode') as string)) {
+      set(this.command, 'loadBalancingPolicy.balancingMode', balancingModes[0]);
+    }
+    return balancingModes;
   }
 
   public $onDestroy (): void {
